343aee3d50183bec1c1a5add7d926378a8fef6a0
Mayesh <josephhernandez9841@gmail.com>
2020-09-28 21:49:44 +0545

    BFS and DFS preorder and postorder

--- BST.js
+++ BST.js
@@ -47,6 +47,39 @@ class BinarySearchTree{
             }
         }
     }
+        BFS(){
+        let node = this.root;
+        let queue = [];
+        let visited = [];
+        queue.push(node);
+        while(queue.length){
+            node = queue.shift();
+            visited.push(node.value);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        return visited;
+    }
+    DFSpreorder(){
+        let visited = [];
+        function traverse(node){
+            visited.push(node.value);
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return visited;
+    }
+    DFSpostorder(){
+        let visited = [];
+        function traverse(node){
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+            visited.push(node.value);
+        }
+        traverse(this.root);
+        return visited;
+    }
 }
 
 let tree = new BinarySearchTree();
